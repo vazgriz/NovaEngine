955f920981c3c078975fb2bb1753449e79c1a73a
Unknown <vazgriz@gmail.com>
2018-06-20 18:11:20 -0500

    Use different constructor syntax

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,7 @@
 #include <NovaEngine/NovaEngine.h>
 
 int main() {
-    Nova::Renderer renderer("Test", {}, { "VK_LAYER_LUNARG_standard_validation" });
-    Nova::Engine engine(std::move(renderer));
+    Nova::Renderer renderer = Nova::Renderer("Test", {}, { "VK_LAYER_LUNARG_standard_validation" });
+    Nova::Engine engine = Nova::Engine(std::move(renderer));
     return 0;
 }
