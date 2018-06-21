e508b42102e912562e7f99b1e4dae0f4fff19945
Unknown <vazgriz@gmail.com>
2018-06-20 21:14:19 -0500

    Use GLFW

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <NovaEngine/NovaEngine.h>
 
 int main() {
+    glfwInit();
+
     Nova::Renderer renderer = Nova::Renderer("Test", {}, { "VK_LAYER_LUNARG_standard_validation" });
     Nova::Engine engine = Nova::Engine(std::move(renderer));
+
+    glfwTerminate();
     return 0;
 }
