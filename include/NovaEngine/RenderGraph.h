21bb4cc7b1cacf5b4ab42446990427963c374e2d
Unknown <vazgriz@gmail.com>
2018-07-02 17:42:44 -0500

    Add events and pipeline barriers

--- include/NovaEngine/RenderGraph.h
+++ include/NovaEngine/RenderGraph.h
@@ -5,25 +5,33 @@
 #include "NovaEngine/Engine.h"
 
 namespace Nova {
+    class RenderGraph;
+
     class RenderNode {
         friend class RenderGraph;
     public:
-        RenderNode(const vk::Queue& queue);
+        RenderNode(RenderGraph* graph, const vk::Queue& queue);
         RenderNode(const RenderNode& other) = delete;
         RenderNode& operator = (const RenderNode& other) = delete;
         RenderNode(RenderNode&& other) = default;
         RenderNode& operator = (RenderNode&& other) = default;
         ~RenderNode() = default;
 
-    protected:
-        const vk::Queue* m_queue;
-        uint32_t m_family;
+        void preRecord(vk::CommandBuffer& commandBuffer, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage) const;
+        void postRecord(vk::CommandBuffer& commandBuffer, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage) const;
 
     private:
+        RenderGraph* m_graph;
+        const vk::Queue* m_queue;
+        uint32_t m_family;
+        std::vector<RenderNode*> m_inNodes;
         std::vector<RenderNode*> m_outNodes;
+        std::vector<vk::Event*> m_inEvents;
+        std::vector<vk::Event*> m_outEvents;
     };
 
     class RenderGraph {
+        friend class RenderNode;
     public:
         RenderGraph(Engine& engine, size_t frames);
         RenderGraph(const RenderGraph& other) = delete;
@@ -41,5 +49,8 @@ namespace Nova {
         std::vector<std::unique_ptr<RenderNode>> m_nodes;
         std::unordered_set<RenderNode*> m_nodeSet;
         std::vector<RenderNode*> m_nodeList;
+        std::vector<std::unique_ptr<vk::Event>> m_events;
+
+        vk::Event& createEvent(const vk::Queue& queue);
     };
 }
--- src/RenderGraph.cpp
+++ src/RenderGraph.cpp
@@ -4,9 +4,31 @@
 
 using namespace Nova;
 
-RenderNode::RenderNode(const vk::Queue& queue) {
+RenderNode::RenderNode(RenderGraph* graph, const vk::Queue& queue) {
+    m_graph = graph;
     m_queue = &queue;
     m_family = queue.familyIndex();
+}
+
+void RenderNode::preRecord(vk::CommandBuffer& commandBuffer, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage) const {
+    for (size_t i = 0; i < m_inEvents.size(); i++) {
+        if (m_outEvents[i] != nullptr) {
+            commandBuffer.waitEvents({ *m_outEvents[i] }, srcStage, dstStage, {}, {}, {});
+            commandBuffer.resetEvent(*m_outEvents[i], dstStage);
+        } else {
+            commandBuffer.pipelineBarrier(srcStage, dstStage, vk::DependencyFlags::None, {}, {}, {});
+        }
+    }
+}
+
+void RenderNode::postRecord(vk::CommandBuffer& commandBuffer, vk::PipelineStageFlags srcStage, vk::PipelineStageFlags dstStage) const {
+    for (size_t i = 0; i < m_inEvents.size(); i++) {
+        if (m_inEvents[i] != nullptr) {
+            commandBuffer.setEvent(*m_inEvents[i], dstStage);
+        } else {
+            commandBuffer.pipelineBarrier(srcStage, dstStage, vk::DependencyFlags::None, {}, {}, {});
+        }
+    }
 }
 
 RenderGraph::RenderGraph(Engine& engine, size_t frames) {
@@ -14,17 +36,39 @@ RenderGraph::RenderGraph(Engine& engine, size_t frames) {
 }
 
 RenderNode& RenderGraph::addNode(const vk::Queue& queue) {
-    m_nodes.emplace_back(std::make_unique<RenderNode>(queue));
+    m_nodes.emplace_back(std::make_unique<RenderNode>(this, queue));
     m_nodeSet.insert(m_nodes.back().get());
     return *m_nodes.back();
 }
 
 void RenderGraph::addEdge(RenderNode& source, RenderNode& dest) {
     source.m_outNodes.push_back(&dest);
+    dest.m_inNodes.push_back(&source);
 }
 
 void RenderGraph::bake() {
     m_nodeList = topologicalSort<RenderNode>(m_nodeSet, [](const RenderNode* node) {
         return node->m_outNodes;
     });
+
+    for (auto node : m_nodeList) {
+        for (auto outNode : node->m_outNodes) {
+            if (node->m_family == outNode->m_family) {
+                vk::Event& event = createEvent(*node->m_queue);
+                node->m_outEvents.push_back(&event);
+                outNode->m_inEvents.push_back(&event);
+            } else {
+                node->m_outEvents.push_back(nullptr);
+                outNode->m_inEvents.push_back(nullptr);
+            }
+        }
+    }
+}
+
+vk::Event& RenderGraph::createEvent(const vk::Queue& queue) {
+    vk::EventCreateInfo info = {};
+
+    m_events.emplace_back(std::make_unique<vk::Event>(queue.device(), info));
+
+    return *m_events.back();
 }
