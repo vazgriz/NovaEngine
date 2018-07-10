d90bef0b0ff51fd16e253dcb9ea061c35c0ef97b
Unknown <vazgriz@gmail.com>
2018-07-09 21:41:38 -0500

    Add StackAllocator

---
+++ include/NovaEngine/StackAllocator.h
@@ -0,0 +1,23 @@
+#include "NovaEngine/GenericAllocator.h"
+#include <vector>
+
+namespace Nova {
+    class StackAllocator : public GenericAllocator {
+    public:
+        StackAllocator(size_t offset, size_t size);
+        StackAllocator(const StackAllocator& other) = delete;
+        StackAllocator& operator = (const StackAllocator& other) = delete;
+        StackAllocator(StackAllocator&& other) = default;
+        StackAllocator& operator = (StackAllocator&& other) = default;
+
+        Allocation allocate(size_t size, size_t alignment) override;
+        void free(Allocation allocation) override;
+        void reset() override;
+
+    private:
+        size_t m_offset;
+        size_t m_size;
+        size_t m_ptr;
+        std::vector<size_t> m_stack;
+    };
+}
---
+++ src/StackAllocator.cpp
@@ -0,0 +1,31 @@
+#include "NovaEngine/StackAllocator.h"
+
+using namespace Nova;
+
+StackAllocator::StackAllocator(size_t offset, size_t size) {
+    m_offset = offset;
+    m_size = size;
+    m_ptr = offset;
+}
+
+Allocation StackAllocator::allocate(size_t size, size_t alignment) {
+    size_t ptr = GenericAllocator::align(m_ptr, alignment);
+    size_t end = ptr + size;
+    if (end <= m_offset + m_size) {
+        m_stack.push_back(m_ptr);
+        m_ptr = end;
+        return { ptr, size };
+    } else {
+        return {};
+    }
+}
+
+void StackAllocator::free(Allocation allocation) {
+    m_ptr = m_stack.back();
+    m_stack.pop_back();
+}
+
+void StackAllocator::reset() {
+    m_stack.clear();
+    m_ptr = m_offset;
+}
