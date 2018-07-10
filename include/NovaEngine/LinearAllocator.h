d4f7868055fe8784a44326657bc10c6a512bfe4f
Unknown <vazgriz@gmail.com>
2018-07-09 21:23:18 -0500

    Add LinearAllocator

---
+++ include/NovaEngine/LinearAllocator.h
@@ -0,0 +1,21 @@
+#include "NovaEngine/GenericAllocator.h"
+
+namespace Nova {
+    class LinearAllocator : public GenericAllocator {
+    public:
+        LinearAllocator(size_t offset, size_t size);
+        LinearAllocator(const LinearAllocator& other) = delete;
+        LinearAllocator& operator = (const LinearAllocator& other) = delete;
+        LinearAllocator(LinearAllocator&& other) = default;
+        LinearAllocator& operator = (LinearAllocator&& other) = default;
+
+        Allocation allocate(size_t size, size_t alignment) override;
+        void free(Allocation allocation) override;
+        void reset() override;
+
+    private:
+        size_t m_offset;
+        size_t m_size;
+        size_t m_ptr;
+    };
+}
---
+++ src/LinearAllocator.cpp
@@ -0,0 +1,28 @@
+#include "NovaEngine/LinearAllocator.h"
+
+using namespace Nova;
+
+LinearAllocator::LinearAllocator(size_t offset, size_t size) {
+    m_offset = offset;
+    m_size = size;
+    m_ptr = offset;
+}
+
+Allocation LinearAllocator::allocate(size_t size, size_t alignment) {
+    size_t ptr = GenericAllocator::align(m_ptr, alignment);
+    size_t end = ptr + size;
+    if (end <= m_offset + m_size) {
+        m_ptr = end;
+        return { ptr, size };
+    } else {
+        return {};
+    }
+}
+
+void LinearAllocator::free(Allocation allocation) {
+    //nothing
+}
+
+void LinearAllocator::reset() {
+    m_ptr = m_offset;
+}
