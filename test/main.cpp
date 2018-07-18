8208cd13da1a67cd1ca829c3c9e14f984e538e2f
Unknown <vazgriz@gmail.com>
2018-07-18 14:30:06 -0500

    Add pipeline and drawing

--- test/main.cpp
+++ test/main.cpp
@@ -3,26 +3,75 @@
 #include <GLFW/glfw3.h>
 #include <NovaEngine/NovaEngine.h>
 #include <glm/glm.hpp>
+#include <fstream>
 
 struct Vertex {
     glm::vec3 pos;
     glm::vec3 color;
+
+    static std::vector<vk::VertexInputAttributeDescription> getAttributes() {
+        vk::VertexInputAttributeDescription att1 = {};
+        att1.binding = 0;
+        att1.format = vk::Format::R32G32B32_Sfloat;
+        att1.location = 0;
+        att1.offset = 0;
+
+        vk::VertexInputAttributeDescription att2 = {};
+        att2.binding = 0;
+        att2.format = vk::Format::R32G32B32_Sfloat;
+        att2.location = 1;
+        att2.offset = sizeof(glm::vec3);
+
+        return { att1, att2 };
+    }
+
+    static std::vector<vk::VertexInputBindingDescription> getBindings() {
+        vk::VertexInputBindingDescription binding = {};
+        binding.binding = 0;
+        binding.inputRate = vk::VertexInputRate::Vertex;
+        binding.stride = sizeof(Vertex);
+
+        return { binding };
+    }
 };
 
 std::vector<Vertex> vertices = {
-    { { -1.0f,  1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f } },
-    { {  0.0f, -1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f } },
-    { {  1.0f,  1.0f, 0.0f }, { 1.0f, 1.0f, 1.0f } }
+    { { -1.0f,  1.0f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
+    { {  0.0f, -1.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+    { {  1.0f,  1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } }
 };
+
+std::vector<char> loadFile(const std::string& fileName) {
+    std::ifstream file = std::ifstream(fileName, std::ios::ate | std::ios::binary);
+    if (!file) {
+        throw std::runtime_error("Could not open file: " + fileName);
+    }
+
+    std::vector<char> result;
+    result.resize(file.tellg());
+
+    file.seekg(0);
+    file.read(result.data(), result.size());
+
+    return result;
+}
+
+vk::ShaderModule loadShader(vk::Device& device, const std::string& fileName) {
+    vk::ShaderModuleCreateInfo info = {};
+    info.code = loadFile(fileName);
+
+    return vk::ShaderModule(device, info);
+}
 
 class TestNode : public Nova::QueueNode {
 public:
     TestNode(const vk::Queue& queue, vk::Swapchain& swapchain, Nova::BufferAllocator& allocator, Nova::TransferNode& transferNode) : QueueNode(queue) {
         m_allocator = &allocator;
         m_transferNode = &transferNode;
-        setSwapchain(swapchain);
         createSemaphores();
         createVertexBuffer();
+        createPipelineLayout();
+        setSwapchain(swapchain);
     }
 
     void setSwapchain(vk::Swapchain& swapchain) {
@@ -30,6 +79,7 @@ public:
         createRenderPass();
         createImageViews();
         createFramebuffers();
+        createPipeline();
     }
 
     vk::Semaphore& acquireSemaphore() { return *m_acquireSemaphore; }
@@ -47,6 +97,8 @@ private:
     std::vector<vk::ImageView> m_imageViews;
     std::vector<vk::Framebuffer> m_framebuffers;
     std::unique_ptr<Nova::Buffer> m_vertexBuffer;
+    std::unique_ptr<vk::PipelineLayout> m_pipelineLayout;
+    std::unique_ptr<vk::Pipeline> m_pipeline;
 
     void createSemaphores() {
         vk::SemaphoreCreateInfo info = {};
@@ -75,6 +127,11 @@ private:
         renderPassInfo.clearValues = { {} };
 
         commandBuffer.beginRenderPass(renderPassInfo, vk::SubpassContents::Inline);
+
+        commandBuffer.bindVertexBuffers(0, { m_vertexBuffer->resource() }, { 0 });
+        commandBuffer.bindPipeline(vk::PipelineBindPoint::Graphics, *m_pipeline);
+        commandBuffer.draw(3, 1, 0, 0);
+
         commandBuffer.endRenderPass();
 
         commandBuffer.end();
@@ -159,6 +216,79 @@ private:
         vk::BufferCopy copy = {};
         copy.size = vertices.size() * sizeof(Vertex);
         m_transferNode->transfer(vertices.data(), *m_vertexBuffer, copy);
+    }
+
+    void createPipelineLayout() {
+        vk::PipelineLayoutCreateInfo info = {};
+
+        m_pipelineLayout = std::make_unique<vk::PipelineLayout>(queue().device(), info);
+    }
+
+    void createPipeline() {
+        vk::ShaderModule vertShader = loadShader(queue().device(), "shader.vert.spv");
+        vk::ShaderModule fragShader = loadShader(queue().device(), "shader.frag.spv");
+
+        vk::PipelineShaderStageCreateInfo vertStage = {};
+        vertStage.stage = vk::ShaderStageFlags::Vertex;
+        vertStage.module = &vertShader;
+        vertStage.name = "main";
+
+        vk::PipelineShaderStageCreateInfo fragStage = {};
+        fragStage.stage = vk::ShaderStageFlags::Fragment;
+        fragStage.module = &fragShader;
+        fragStage.name = "main";
+
+        vk::PipelineVertexInputStateCreateInfo vertexInput = {};
+        vertexInput.vertexAttributeDescriptions = Vertex::getAttributes();
+        vertexInput.vertexBindingDescriptions = Vertex::getBindings();
+
+        vk::PipelineInputAssemblyStateCreateInfo inputAssembly = {};
+        inputAssembly.topology = vk::PrimitiveTopology::TriangleList;
+
+        vk::Viewport viewport = {};
+        viewport.width = static_cast<float>(m_swapchain->extent().width);
+        viewport.height = static_cast<float>(m_swapchain->extent().height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+        vk::Rect2D scissor = {};
+        scissor.extent = m_swapchain->extent();
+
+        vk::PipelineViewportStateCreateInfo viewportState = {};
+        viewportState.viewports = { viewport };
+        viewportState.scissors = { scissor };
+
+        vk::PipelineRasterizationStateCreateInfo rasterizer = {};
+        rasterizer.polygonMode = vk::PolygonMode::Fill;
+        rasterizer.lineWidth = 1.0f;
+        rasterizer.cullMode = vk::CullModeFlags::Back;
+        rasterizer.frontFace = vk::FrontFace::Clockwise;
+
+        vk::PipelineMultisampleStateCreateInfo multisampling = {};
+        multisampling.rasterizationSamples = vk::SampleCountFlags::_1;
+
+        vk::PipelineColorBlendAttachmentState blendAttachment = {};
+        blendAttachment.colorWriteMask = vk::ColorComponentFlags::R |
+                                         vk::ColorComponentFlags::G |
+                                         vk::ColorComponentFlags::B |
+                                         vk::ColorComponentFlags::A;
+
+        vk::PipelineColorBlendStateCreateInfo blending = {};
+        blending.attachments = { blendAttachment };
+
+        vk::GraphicsPipelineCreateInfo info = {};
+        info.stages = { vertStage, fragStage };
+        info.vertexInputState = &vertexInput;
+        info.inputAssemblyState = &inputAssembly;
+        info.viewportState = &viewportState;
+        info.rasterizationState = &rasterizer;
+        info.multisampleState = &multisampling;
+        info.colorBlendState = &blending;
+        info.layout = m_pipelineLayout.get();
+        info.renderPass = m_renderPass.get();
+        info.subpass = 0;
+
+        m_pipeline = std::make_unique<vk::GraphicsPipeline>(queue().device(), info);
     }
 };
 
---
+++ test/shader.vert
@@ -0,0 +1,16 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 col;
+
+layout(location = 0) out vec3 vColor;
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+void main() {
+    gl_Position = vec4(pos, 1.0);
+    vColor = col;
+}
