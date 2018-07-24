23973587dc931c75d093cbdc79f0c46841416470
Unknown <vazgriz@gmail.com>
2018-07-24 12:10:27 -0500

    Move QueueGraph to Engine

--- include/NovaEngine/Engine.h
+++ include/NovaEngine/Engine.h
@@ -3,6 +3,7 @@
 #include "NovaEngine/Renderer.h"
 #include "NovaEngine/Window.h"
 #include "NovaEngine/Memory.h"
+#include "NovaEngine/QueueGraph.h"
 
 namespace Nova {
     class Engine {
@@ -16,12 +17,14 @@ namespace Nova {
         Renderer& renderer() { return *m_renderer; }
         Memory& memory() { return *m_memory; }
         Window& window() { return *m_window; }
+        QueueGraph& queueGraph() { return *m_queueGraph; }
 
     private:
         friend class Window;
         Renderer* m_renderer = nullptr;
         Window* m_window = nullptr;
         std::unique_ptr<Memory> m_memory;
+        std::unique_ptr<QueueGraph> m_queueGraph;
 
         void addWindow(Window& window);
     };
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -5,6 +5,7 @@ using namespace Nova;
 Engine::Engine(Renderer& renderer) {
     m_renderer = &renderer;
     m_memory = std::make_unique<Memory>(*this);
+    m_queueGraph = std::make_unique<QueueGraph>(*this);
 }
 
 void Engine::addWindow(Window& window) {
@@ -13,4 +14,5 @@ void Engine::addWindow(Window& window) {
     }
 
     m_window = &window;
+    m_queueGraph->setFrames(m_window->swapchain().images().size());
 }
