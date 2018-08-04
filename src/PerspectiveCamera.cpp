5148336f6f2aa906f76b31652f7e86021fb2ec07
Unknown <vazgriz@gmail.com>
2018-08-03 20:48:44 -0500

    Remove correction matrix
    Since the matrix formula already maps Z to [0.0, 1.0], we only need to flip the y axis

--- src/PerspectiveCamera.cpp
+++ src/PerspectiveCamera.cpp
@@ -4,24 +4,18 @@
 
 using namespace Nova;
 
-const glm::mat4 correctionMatrix = {
-    1, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, -0.5f, 0,
-    0, 0, 0.5f, 1
-};
-
 PerspectiveCamera::PerspectiveCamera(Engine& engine, CameraManager& cameraManager, BufferAllocator& allocator, glm::ivec2 size, float fov)
     : Camera(engine, cameraManager, allocator, size) {
     m_fov = fov;
 }
 
 glm::mat4 PerspectiveCamera::getProjection() {
+    //http://dev.theomader.com/depth-precision/
     float f = 1.0f / tan(glm::radians(m_fov) / 2.0f);
     float aspect = size().x / static_cast<float>(size().y);
     glm::mat4 projection = {
         f / aspect, 0, 0, 0,
-        0, f, 0, 0,
+        0, -f, 0, 0,
         0, 0, 0, -1,
         0, 0, 0.1f, 0
     };
